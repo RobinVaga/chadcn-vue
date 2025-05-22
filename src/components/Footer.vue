a7ab0c53b904c1ea46f1b0026ce58825719c852b
Robin <robin.vaga@ametikool.ee>
2025-05-22 09:00:20 +0300

    Finalizing the footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,76 @@
+<template>
+    <footer class ="bg-white text-black pt-1 pb-10 px-6 md:px-12"></footer>
+
+   
+
+    <div class="bg-slate-100 text-gray-500 pt-10 pl-25 pb-10 grid grid-cols-1 md:grid-cols-5 gap-8 text-sm">
+
+<!-- Logo -->
+<div class="space-y-3">
+    <p class="text-green-700">Home</p>
+    <p>My favorites</p>
+    <p>Recents</p>
+</div>
+
+<!-- Links Column 1 -->
+<div class="space-y-3">
+  <p class="text-black">Explore</p>
+  <p>Lists</p>
+  <p>Maps
+    <span class="ml-2 bg-lime-100 text-lime-700 text-xs font-medium px-2 py-0.5 rounded-full">
+      New
+    </span>
+  </p>
+</div>
+
+<!-- Links Column 2 -->
+<div class="space-y-3">
+  <p class="text-black">Campaigns</p>
+  <p>Home</p>
+  <p>Donations
+    <span class="ml-2 bg-lime-100 text-lime-700 text-xs font-medium px-2 py-0.5 rounded-full">
+      4
+    </span>
+  </p>
+  <p>Campaigns
+    <span class="ml-2 bg-lime-100 text-lime-700 text-xs font-medium px-2 py-0.5 rounded-full">
+      12
+    </span>
+  </p>
+  <p>Analytics</p>
+</div>
+
+<div class="space-y-3">
+  <p class="text-black">Profile</p>
+  <p>Settings</p>
+  <p>Notifications
+    <span class="ml-2 bg-lime-100 text-lime-700 text-xs font-medium px-2 py-0.5 rounded-full">
+      10
+    </span>
+
+  </p>
+</div>
+
+<!-- Links Column 3 -->
+<div class="space-y-3">
+  <p class="text-black">Resources</p>
+  <p>How to use Crowdfy</p>
+  <p>Docs</p>
+  <p>Legal terms</p>
+  <p>Blog</p>
+  <p>Merch</p>
+</div>
+</div>
+
+
+<!-- Bottom Credits and Social -->
+<div class="bg-slate-100 border-t border-gray-400 mt-0 flex justify-between items-center text-xs text-gray-400 gap-1 pt-6 pb-6 px-6">
+    <div class="flex items-center space-x-2">
+        <img src="../assets/Logo.png" alt="logo" class="h-6 " />
+    </div>
+    <p>© 2077 Mainnet Design. All rights reserved.</p>
+</div>
+
+
+
+</template>
